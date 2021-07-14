55609b54b88be6aad62c3f291135865045abff18
rohanlahori <64496391+rohanlahori@users.noreply.github.com>
2021-07-14 09:55:46 +0530

    Create dfs1.cpp

---
+++ Graph/dfs1.cpp
@@ -0,0 +1,78 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <algorithm>
+using namespace std;
+#define ll long long 
+#define all(x) (x).begin(),(x).end()
+#define vll vector<ll>
+#define vi vector<ll>
+#define vp vector<pi>
+#define vs vector<string>
+#define pb push_back
+#define mp make_pair
+#define POB pop_back 
+#define f(i,x,z) for(ll i=x;i<z;i++)
+#define debug(x) cout<<'DEBUG------> '<<x<<endl
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define clr(x) memset(x, 0, sizeof(x)) 
+#define setall(x,a) memset(x, a, sizeof(x))
+#define INF 1000000000000000003 
+#define mathpi 3.141592653589793238 
+#define F first;
+#define S second;
+#define vpair vector<pair<ll,ll>>
+#define vpairs vector<pair<string,string>>
+#define tin tuple<ll,ll,ll>
+#define rev(i,x,z) for(ll i=x;i>=z;i--)  
+#define ld long double 
+#define print(v) for(auto x:v){cout<<x<<"\n";}cout<<"\n"; 
+#define print_pair(v) for(auto x:v){cout<<x.first<<" "<<x.second<<" ";}cout<<"\n"; 
+#define reverse(v) reverse(v.begin(),v.end()) 
+#define nline "\n"
+#define nl "\n"
+
+vector<ll> visited(10000,0);
+vector<vi>adj;
+void dfs(ll node)
+{
+    cout<<node<<" ";
+    visited[node]=1;
+    for(auto x:adj[node])
+    {
+        if(!visited[x])
+        {
+            visited[x]=1;
+            dfs(x);
+        }
+    }
+}
+int main()
+{
+	// freopen("inp.txt","r",stdin);
+	// freopen("out.txt","w",stdout);
+	ios::sync_with_stdio(0);// Input and output clearance
+	cin.tie(0);
+    adj.resize(100000);
+    ll n;
+    cin>>n;
+    f(i,0,n-1)
+    {
+        ll u,v;
+        cin>>u>>v;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    f(i,1,n+1)
+    {
+        if(!visited[i])
+        {
+            dfs(i);
+        }
+    }
+
+}
+
