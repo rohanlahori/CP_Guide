aec8e6c2c0441f9dd81695c2417c587326b145a9
Rohan Lahori <64496391+rohanlahori@users.noreply.github.com>
2022-06-30 23:59:59 +0530

    Update Sieve.cpp

--- Algorithms/Sieve.cpp
+++ Algorithms/Sieve.cpp
@@ -2,7 +2,7 @@ void sieve(ll n)
 {
     vector<char> is_prime(n+1, true);
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i <= n; i++) 
+    for (int j = 2; i <= n; i++) 
     {
         if (is_prime[i] && (long long)i * i <= n) 
         {
