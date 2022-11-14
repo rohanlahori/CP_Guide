9738e974332709a97e8c8d3b361aab7a67fdd105
Rohan Lahori <64496391+rohanlahori@users.noreply.github.com>
2022-11-15 00:10:20 +0530

    Create leetcode_template.cpp

---
+++ leetcode_template.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <algorithm>
+using namespace std;
+#define ll long long 
+#define all(x) (x).begin(),(x).end()
+#define vll vector<ll>
+#define vi vector<ll>
+#define vp vector<pi>
+#define vs vector<string>
+#define pb push_back
+#define mp make_pair
+#define POB pop_back 
+#define f(i,x,z) for(ll i=x;i<z;i++)
+#define debug(x) cout<<'DEBUG------> '<<x<<endl
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define clr(x) memset(x, 0, sizeof(x)) 
+#define setall(x,a) memset(x, a, sizeof(x))
+#define INF 1000000000000000003 
+#define mathpi 3.141592653589793238 
+#define F first;
+#define S second;
+#define vpair vector<pair<ll,ll>>
+#define vpairs vector<pair<string,string>>
+#define tin tuple<ll,ll,ll>
+#define rev(i,x,z) for(ll i=x;i>=z;i--)  
+#define ld long double 
+#define print(v) for(auto x:v){cout<<x<<" ";}cout<<"\n"; 
+#define print_pair(v) for(auto x:v){cout<<x.first<<" "<<x.second<<" ";}cout<<"\n"; 
+#define reverse(v) reverse(v.begin(),v.end()) 
+#define nline "\n"
+#define nl "\n"
+
+
+class Solution {
+public:
+
+    int leetcode(vector<int> &nums)
+    {
+        return 0;
+    }
+};
+
+
+int main()
+{
+    Solution obj;
+    ll n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>nums[i];
+    }
+    cout<<obj.leetcode(nums);
+}
